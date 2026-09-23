aa43e7d1cc9b4831a81ec1f5f1687dde125c593e
nithinp661 <nithinp6161.com>
2026-09-23 19:47:18 +0530

    added backed.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,10 @@ terraform {
     storage_account_name = "terrastateanup01"
     container_name       = "storestatefile"
     key                  = "appservice/dev.tfstate"
+
+    use_oidc             = true
+    use_azuread_auth     = true
+    tenant_id            = "fba9c407-61bd-49e5-b51a-9d8e7915e91f"
+    client_id            = "723d093b-459c-4c71-a217-a6ce5e931b04"
   }
 }
